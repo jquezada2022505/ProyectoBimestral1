f9b4d2e917d0718e1c52c559069c80cc31362dc3
jquezada2022505 <jquezada-2022505@kinal.edu.gt>
2024-02-17 03:53:42 -0600

    Mensaje de confirmación

--- models/server.js
+++ models/server.js
@@ -2,35 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../db/config');
 
-class Server{
-    
-    constructor(){
+class Server {
+
+    constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        //this.usuarioPath = '/api/usuarios';
-        //this.authPath = '/api/auth';
+        this.usuarioPath = '/api/usuarios';
+        this.authPath = '/api/auth';
         this.conectarDB();
         this.middlewares();
         this.routes();
     }
 
-    async conectarDB(){
+    async conectarDB() {
         await dbConnection();
     }
 
-    middlewares(){
+    middlewares() {
         this.app.use(express.static('public'));
         this.app.use(cors());
         this.app.use(express.json());
     }
 
-    routes(){
-        //this.app.use(this.authPath, require('../routes/auth.routes'));
-        //this.app.use(this.usuarioPath, require('../routes/user.routes'));
+    routes() {
+        this.app.use(this.authPath, require('../routes/auth.routes'));
+        this.app.use(this.usuarioPath, require('../routes/user.routes'));
     }
 
-    listen(){
-        this.app.listen(this.port, () =>{
+    listen() {
+        this.app.listen(this.port, () => {
             console.log('Servidor ejecutandose y escuchando el puerto', this.port)
         });
     }
